2cc07a2d3cf5f5943508067ece44b03719a46b59
Victor Rodrigues <viictoorrodrigues15@gmail.com>
2022-05-10 22:04:15 -0300

    att render
    att render up

--- src/models/Pokemon.js
+++ src/models/Pokemon.js
@@ -1,35 +1,44 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/db");
 
-const Pokemon = connection.define("pokemon", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const Pokemon = connection.define(
+  "pokemon",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    nome: {
+      type: Sequelize.STRING,
+      autoIncrement: false,
+    },
+    descricao: {
+      type: Sequelize.STRING,
+      autoIncrement: false,
+    },
+    tipo: {
+      type: Sequelize.STRING,
+      autoIncrement: false,
+    },
+    imagem: {
+      type: Sequelize.STRING,
+      autoIncrement: false,
+    },
   },
-  nome: {
-    type: Sequelize.STRING,
-    autoIncrement: false,
-  },
-  descricao: {
-    type: Sequelize.STRING,
-    autoIncrement: false,
-  },
-  tipo: {
-    type: Sequelize.STRING,
-    autoIncrement: false,
-  },
-  imagem: {
-    type: Sequelize.STRING,
-    autoIncrement: false,
-  },
-}, {
+  {
     freezeTableName: true,
     timestamps: false,
     createdAt: false,
     updateAt: false,
-   }
+  }
 );
+
+const initTable = async () => {
+  await Pokemon.sync();
+};
+
+initTable();
 
 module.exports = Pokemon;
